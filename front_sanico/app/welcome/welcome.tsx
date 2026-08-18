eabd1a65b667ba67af31d4e312e0a9d49b66a8cc
Kauan Rodrigues <rodrigueskauan2022@gmail.com>
2026-08-18 08:44:18 -0300

    feat: :sparkles: Tpo resolvido
    topo feito

--- front_sanico/app/welcome/welcome.tsx
+++ front_sanico/app/welcome/welcome.tsx
@@ -22,43 +22,38 @@ export function Welcome() {
       </div>
         
 
-        <div className="">
-        <div className=" flex flex-1   justify-between ">
-          <div className="text-2xl font-bold justify-around flex gap-50 bg-white full-w min-h-full p-4 rounded-lg shadow-lg">
+        <div className="flex-1 flex flex-col">
+          <div className="w-full bg-white p-4 shadow-lg flex justify-between items-center">
             <div>
-               Welcome back,Lohran!
-              <p className="text-lg text-gray-100">
-                blablabla
-              </p>
-
+              <h1 className="text-2xl font-bold">Welcome back, Lohran!</h1>
+              <p className="text-lg text-gray-500">blablabla</p>
             </div>
-
-              <div className="">
-                <input type="text" placeholder="Pesquisar..." className="  bg-gray-200 border-px-0  text-sm  rounded-lg w-110 h-10" />
+          
+            <div>
+              <input
+                type="text"
+                placeholder="Pesquisar..."
+                className="bg-gray-200 text-sm rounded-lg w-80 h-10 px-4"
+              />
+            </div>
+          
+            <div className="flex items-center gap-4">
+              <h1 className="text-lg font-semibold">Notificação</h1>
+              <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center">
+                <img
+                  src="https://via.placeholder.com/40"
+                  alt="Profile"
+                  className="rounded-full"
+                />
               </div>
-
-              <div className="flex gap-4 ">
-                <div className="">
-                  <h1 className="text-lg font-semibold  ">Notificação</h1>
-                </div>
-                <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center">
-                  <img src="https://via.placeholder.com/40" alt="Profile" className="rounded-full" />
-                </div>
-                <span className="text-lg font-semibold">Lohran</span>
-                
-                 
-              </div>
-              
+              <span className="text-lg font-semibold">Lohran</span>
+            </div>
           </div>
-       
-       </div>
-
-      <div className="flex-1 flex bg-gray-600 p-4 rounded-lg shadow-lg m-4">
-        gb
-
-      </div>
-
-      </div>
+          
+          <div className="flex-1 bg-gray-600 p-4 rounded-lg shadow-lg m-4">
+            gb
+          </div>
+        </div>
       
     </main>
   );
