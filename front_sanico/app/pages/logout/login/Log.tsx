4f9d2ca4abaa3df714886fa927e6de4a3e543572
kauan <rodrigueskauan@gmail.com>
2026-09-08 11:15:33 -0300

    feat: retirar o "oi" do codigo do logout

--- front_sanico/app/pages/logout/login/Log.tsx
+++ front_sanico/app/pages/logout/login/Log.tsx
@@ -20,7 +20,7 @@ export function Log() {
   const navigate = useNavigate();
   return (
     <main className="app-shell">
-      <h1>oi</h1>
+      
       <aside className="app-sidebar">
         <div className="flex items-center gap-3 mb-10">
           <div className="app-logo-mark">
